d37d8446c164cecfdb69131cdd704f376e5f7c9b
fr-matsuo <matsuo@fact-real.com>
2014-06-09 01:31:47 +0000

    確認画面に入力値を表示する処理 #40

--- app/views/form.blade.php
+++ app/views/form.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('title')
-  <title>フォーム画面</title>
+  フォーム画面
 @stop
 
 @section('context')
@@ -13,8 +13,8 @@
       <legend>フォーム</legend>
       
       <label>名前:</label>
-      <input type="text" name="name_first" id="name_first">
-      <input type="text" name="name_last" id="name_last"> 
+      <input type="text" name="name_first" id="name_first" value={{{ Input::get('name_first') }}}>
+      <input type="text" name="name_last" id="name_last" value={{{ Input::get('name_last') }}}> 
       <br> 
 
       <label>性別:</label>
@@ -23,13 +23,13 @@
       <br>
 
       <label>郵便番号:</label>
-      <input type="text" name="post_first" id="post_first">
+      <input type="text" name="post_first" id="post_first" value={{{ Input::get('post_first') }}}>
       -
-      <input type="text" name="post_last" id="post_last">
+      <input type="text" name="post_last" id="post_last" value={{{ Input::get('post_first') }}}>
       <br>
 
       <label>都道府県:</label>
-      <select name="prefecture" id="prefecture" size=1>
+      <select name="prefecture" id="prefecture" size=1 value={{{ Input::get('prefecture') }}}>
         <option id="--" value="--">--</option>
         <option id="Tokyo" value="Tokyo">東京都</option>
         <option id="Saitama" value="Saitama">埼玉県</option>
@@ -38,13 +38,13 @@
       <br>
 
       <label>メールアドレス:</label>
-      <input type="text" name="mail_address" id="mail">
+      <input type="text" name="mail_address" id="mail" value={{{ Input::get('mail_address') }}}>
       <br>
 
       <label>趣味</label>
-        <input type="checkbox" name="hobby" id="music" value="music"><label for="music">音楽鑑賞</label>
-        <input type="checkbox" name="hobby" id="movie" value="movie"><label for="movie">映画鑑賞</label>
-        <input type="checkbox" name="hobby" id="other" value="other"><label for="other">その他</label>
+        <input type="checkbox" name="hobby[]" id="music" value="music"><label for="music">音楽鑑賞</label>
+        <input type="checkbox" name="hobby[]" id="movie" value="movie"><label for="movie">映画鑑賞</label>
+        <input type="checkbox" name="hobby[]" id="other" value="other"><label for="other">その他</label>
         <input type="text" name="other_descript" id="other_descript">
       </label>
       <br>
--- app/views/formCheck.blade.php
+++ app/views/formCheck.blade.php
@@ -8,20 +8,65 @@
   <h1>フォーム>確認</h1>
   
   <form method="post">
-    <p>名前：山田太郎</p>
+    <p>
+      名前：
+      {{{ Input::get('name_first') }}}
+      {{  ' ' }}
+      {{{ Input::get('name_last') }}}
+    </p>
     
-    <p>性別:男性</p>
+    <p>
+      性別:
+      {{{ Input::get('sex') }}}
+    </p>
     
-    <p>郵便番号:150-0002</p>
+    <p>
+      郵便番号:
+      {{{ Input::get('post_first') }}}
+      {{  '-' }}
+      {{{ Input::get('post_last') }}}
+    </p>
     
-    <p>都道府県:東京都</p>
+    <p>
+      都道府県:
+      {{{ Input::get('prefecture') }}}
+    </p>
     
-    <p>メールアドレス:test@fact-real.com</p>
+    <p>
+      メールアドレス:
+      {{{ Input::get('mail_address') }}}
+    </p>
     
-    <p>趣味:その他(サッカー)</p>
+    <p>
+      趣味:
+      @if ( empty(Input::get('hobby')) == false )
+      
+      @foreach (Input::get('hobby') as $hobby) 
+      {{{ $hobby }}}
+      {{  ' ' }}
+      @endforeach
+
+      @if ( in_array('other', Input::get('hobby')) )
+      {{  '(' }}
+      {{{ Input::get('other_descript') }}}
+      {{  ')' }}
+      @endif
+
+      @endif
+    </p>
     
-    <p>ご意見:よろしくお願いします</p>
+    <p>
+      ご意見:
+      {{{ Input::get('opinion') }}}
+    </p>
     
+    <input type="hidden" name="name_first" value={{{Input::get('name_first')}}}>
+    <input type="hidden" name="name_last" value={{{Input::get('name_last')}}}>
+    <input type="hidden" name="sex" value={{{Input::get('sex')}}}>
+    <input type="hidden" name="post_first" value={{{Input::get('post_first')}}}>
+    <input type="hidden" name="post_first" value={{{Input::get('post_last')}}}>
+    <input type="hidden" name="mail_address" value={{{Input::get('mail_address')}}}>
+
     <input type="submit" value="送信" formaction="finish">
     <input type="submit" value="戻る" formaction="form">
   </form>
