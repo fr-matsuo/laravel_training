6604ba9eb4cfcdd33436967e28594e550f095608
fr-matsuo <matsuo@fact-real.com>
2014-06-10 01:41:27 +0000

    formCheckをLaravelで記述 #40

--- app/views/formCheck.blade.php
+++ app/views/formCheck.blade.php
@@ -6,75 +6,88 @@
 
 @section('context')
   <h1>フォーム>確認</h1>
+
+  <p>
+    名前：
+    {{{ Input::get('name_first') }}}
+    {{{ Input::get('name_last') }}}
+  </p>
   
-  <form method="post">
-    <p>
-      名前：
-      {{{ Input::get('name_first') }}}
-      {{  ' ' }}
-      {{{ Input::get('name_last') }}}
-    </p>
-    
-    <p>
-      性別:
-      {{{ Input::get('sex') }}}
-    </p>
-    
-    <p>
-      郵便番号:
-      {{{ Input::get('post_first') }}}
-      {{  '-' }}
-      {{{ Input::get('post_last') }}}
-    </p>
-    
-    <p>
-      都道府県:
-      {{{ Input::get('prefecture') }}}
-    </p>
-    
-    <p>
-      メールアドレス:
-      {{{ Input::get('mail_address') }}}
-    </p>
-    
-    <p>
-      趣味:
-      @if ( empty(Input::get('hobby')) == false )
-      
+  <p>
+    性別:
+    {{{ Input::get('sex') }}}
+  </p>
+  
+  <p>
+    郵便番号:
+    {{{ Input::get('post_first') }}}
+    {{  '-' }}
+    {{{ Input::get('post_last') }}}
+  </p>
+  
+  <p>
+    都道府県:
+    {{{ Input::get('prefecture') }}}
+  </p>
+  
+  <p>
+    メールアドレス:
+    {{{ Input::get('mail_address') }}}
+  </p>
+  
+  <p>
+    趣味:
+    @if ( empty(Input::get('hobby')) == false ) 
       @foreach (Input::get('hobby') as $hobby) 
-      {{{ $hobby }}}
-      {{  ' ' }}
+        {{{ $hobby }}}
       @endforeach
 
       @if ( in_array('その他', Input::get('hobby')) )
-      {{  '(' }}
-      {{{ Input::get('other_descript') }}}
-      {{  ')' }}
+        {{  '(' }}
+        {{{ Input::get('other_descript') }}}
+        {{  ')' }}
       @endif
-
-      @endif
-    </p>
-    
-    <p>
-      ご意見:
-      {{{ Input::get('opinion') }}}
-    </p>
-    
-    <input type="hidden" name="name_first" value={{{ Input::get('name_first') }}}>
-    <input type="hidden" name="name_last" value={{{ Input::get('name_last') }}}>
-    <input type="hidden" name="sex" value={{{ Input::get('sex') }}}>
-    <input type="hidden" name="post_first" value={{{ Input::get('post_first') }}}>
-    <input type="hidden" name="post_last" value={{{ Input::get('post_last') }}}>
-    <input type="hidden" name="prefecture" value={{{ Input::get('prefecture') }}}>
-    <input type="hidden" name="mail_address" value={{{ Input::get('mail_address') }}}>
-    @if (empty(Input::get('hobby')) == false)
-    @foreach (Input::get('hobby') as $hobby)
-    <input type="hidden" name="hobby[]" value={{{ $hobby }}}>
-    @endforeach
     @endif
-    <input type="hidden" name="other_descript" value={{{ Input::get('other_descript') }}}>
-    <input type="hidden" name="opinion" value={{{ Input::get('opinion') }}}>
-    <input type="submit" value="送信" formaction="finish">
-    <input type="submit" value="戻る" formaction="form">
-  </form>
+  </p>
+  
+  <p>
+    ご意見:
+    {{{ Input::get('opinion') }}}
+  </p>
+  
+  {{ Form::open(array('url' => 'form/finish')) }}
+    {{ Form::hidden('name_first', Input::get('name_first')) }}
+    {{ Form::hidden('name_last', Input::get('name_last')) }}
+    {{ Form::hidden('sex', Input::get('sex')) }}
+    {{ Form::hidden('post_first', Input::get('post_first')) }}
+    {{ Form::hidden('post_last', Input::get('post_last')) }}
+    {{ Form::hidden('prefecture', Input::get('prefecture')) }}
+    {{ Form::hidden('mail_address', Input::get('mail_address')) }}
+    @if (empty(Input::get('hobby')) == false)
+      @foreach (Input::get('hobby') as $hobby)
+        {{ Form::hidden('hobby[]', $hobby) }}
+      @endforeach
+    @endif
+    {{ Form::hidden('other_descript', Input::get('other_descript')) }}
+    {{ Form::hidden('opinion', Input::get('opinion')) }}
+    {{ Form::submit('送信') }}
+  {{ Form::close() }}
+  
+  {{ Form::open(array('url' => 'form/form')) }}
+    {{ Form::hidden('name_first', Input::get('name_first')) }}
+    {{ Form::hidden('name_last', Input::get('name_last')) }}
+    {{ Form::hidden('sex', Input::get('sex')) }}
+    {{ Form::hidden('post_first', Input::get('post_first')) }}
+    {{ Form::hidden('post_last', Input::get('post_last')) }}
+    {{ Form::hidden('prefecture', Input::get('prefecture')) }}
+    {{ Form::hidden('mail_address', Input::get('mail_address')) }}
+    @if (empty(Input::get('hobby')) == false)
+      @foreach (Input::get('hobby') as $hobby)
+        {{ Form::hidden('hobby[]', $hobby) }}
+      @endforeach
+    @endif
+    {{ Form::hidden('other_descript', Input::get('other_descript')) }}
+    {{ Form::hidden('opinion', Input::get('opinion')) }}
+    {{ Form::submit('戻る') }}
+  {{ Form::close() }}
 @stop
