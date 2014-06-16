062ca2c7b7308e72e6300edd3bc2fca2c93bd670
fr-matsuo <matsuo@fact-real.com>
2014-06-16 07:52:09 +0000

    確認用処理の削除 #43

--- app/helpers.php
+++ app/helpers.php
@@ -17,19 +17,3 @@ function getSelectedText($input, $search, $return) {
 
     return '';
 }
-
-//DB表示確認用、現在未使用
-function showTable() {
-    $show_columns = array(
-        'user_id', 'first_name', 'last_name', 'email', 'pref_id', 'created_at', 'updated_at'
-    );
-
-    $mat = Account_Info::all();
-
-    foreach ($mat as $record) {
-        print "<br>";
-        foreach ($show_columns as $column) {
-            printf("%s\t", $record[$column]);
-        }
-    }
-}
--- app/views/finish.blade.php
+++ app/views/finish.blade.php
@@ -7,12 +7,7 @@
 @section('context')
   <h1>フォーム>完了</h1>
 
-  
-  <?php 
-    //account_info出力、動作確認用
-    //showTable();
-  ?>
-
   応募しました
+ 
   <p><a href="index">TOPページへ</a><p>
 @stop
