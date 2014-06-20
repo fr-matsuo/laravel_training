b7437c9ff64c8b0d13ef9d403a4da40140d4b28e
fr-matsuo <matsuo@fact-real.com>
2014-06-20 04:55:03 +0000

    エラー時の遷移をチェック #44

--- app/tests/FormControllerTest.php
+++ app/tests/FormControllerTest.php
@@ -1,15 +1,43 @@
 <?php
 
 class FormControllerTest extends TestCase {
-    private $_controller = null;
 
-    private function _getController() {
-        if($this->_controller == null) {
-            $this->_controller = new FormController();
-        }
-        return $this->_controller;
+    public function testNormalAction() {
+       $response = $this->action('GET', 'FormController@getIndex');
     }
 
-    //テストがなくて失敗するエラー回避用
-    public function testDummy() {}
+    public function testErrorValidation() {
+        $illegal_input = array(
+            'name_first'     => '',
+            'name_last'      => '',
+            'sex'            => '',
+            'post_first'     => '',
+            'post_last'      => '',
+            'mail_address'   => '',
+            'hobby'          => array(),
+            'other_descript' => '',
+            'opinion'        => ''
+        );
+
+        $response = $this->action('POST', 'FormController@postFormcheck', array(), $illegal_input);
+        $this->assertRedirectedTo('form/form');
+    }
+
+    public function testErrorAddRecord() {
+        //DBに登録済みのアカウントデータ
+        $added_account_data = array(
+            'name_first'     => 'add',
+            'name_last'      => 'record',
+            'sex'            => '男性',
+            'post_first'     => '000',
+            'post_last'      => '0000',
+            'mail_address'   => 'test@data.com',
+            'prefecture'     => 44,
+            'other_descript' => '',
+            'opinion'        => ''
+        );
+        
+        $response = $this->action('POST', 'FormController@postFinish', array(), $added_account_data);
+        $this->assertEquals('form', $response->original->getName());
+    }
 }
